a8762dd48ad97ea1397391ff492eb84d34dd1474
unknown <ymcsoln420@gmail.com>
2025-09-01 12:14:17 +0300

    added jwt secure

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // true in production
+    secure: true,
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dys
     // domain : process.env.NODE_ENV === "production" ? "suuqlay.com" :"localhost"
